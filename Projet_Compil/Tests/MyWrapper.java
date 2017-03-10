ffc92dca73b08443defd156b58044a683b7f9732
lauriehouseaux <lma.houseaux@gmail.com>
2017-03-10 05:03:58 +0100

    modification de MyWrapper pour que tous les tests s'affichent + explications dans readme

--- Projet_Compil/Tests/MyWrapper.java
+++ Projet_Compil/Tests/MyWrapper.java
@@ -5,9 +5,16 @@ public class MyWrapper {
     public static void main(String[] args){
 
         int i;
-        i = testsJasmin.even(Integer.parseInt(args[0]));
+        i = testsJasmin1.even(Integer.parseInt(args[0]));
         System.out.println("result: "+ i);
+	
+	int j;
+        j = testsJasmin2.even(Integer.parseInt(args[0]));
+        System.out.println("result: "+ j);
 
+	int k;
+        k = testsJasmin1.even(Integer.parseInt(args[0]));
+        System.out.println("result: "+ k);
 
     }
 
